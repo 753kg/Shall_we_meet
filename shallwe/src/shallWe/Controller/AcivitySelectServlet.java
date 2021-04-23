bbc1bd4feeadbd65644a46bdeb8763094ae6c5df
rladbdus1231 <60176735+rladbdus1231@users.noreply.github.com>
2021-04-23 18:03:19 +0900

    제발부탁드립니다

--- shallwe/src/shallWe/Controller/AcivitySelectServlet.java
+++ shallwe/src/shallWe/Controller/AcivitySelectServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class AcivitySelect dd
+ * Servlet implementation class AcivitySelect ¾È³ç
  */
 @WebServlet("/AcivitySelect")
 public class AcivitySelectServlet extends HttpServlet {
