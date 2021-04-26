7c8e1ce2be5af9f9113fece6ee037d66e6fc0226
MyGram <753kg@naver.com>
2021-04-26 21:35:59 +0900

    java: add register function

--- shallwe/src/shallWe/Controller/RegisterServlet.java
+++ shallwe/src/shallWe/Controller/RegisterServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import shallWe.DAO.MemberDAO;
 import shallWe.VO.MemberVO;
 
 /**
@@ -17,14 +18,29 @@ public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
      
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String register_id = (String) request.getAttribute("register_id");
-		String register_pw = (String) request.getAttribute("register_pw");
-		String register_name = (String) request.getAttribute("register_name");
-		String register_email = (String) request.getAttribute("register_email");
-		String register_phone = (String) request.getAttribute("register_phone");
-		String register_security = (String) request.getAttribute("register_security");
+		String register_id = request.getParameter("register_id");
+		String register_pw = request.getParameter("register_pw");
+		String register_name = request.getParameter("register_name");
+		String register_email = request.getParameter("register_email");
+		String register_phone = request.getParameter("register_phone");
+		String register_security = request.getParameter("register_security");
 		
 		MemberVO new_mem = new MemberVO();
+		new_mem.setMember_id(register_id);
+		new_mem.setPassword(register_pw);
+		new_mem.setName(register_name);
+		new_mem.setEmail(register_email);
+		new_mem.setPhone_number(register_phone);
+		new_mem.setSecurity(register_security);
+		
+		MemberDAO dao = new MemberDAO();
+		int result = dao.insertMember(new_mem);
+		String message = "회원가입 성공";
+		if(result == 0) {
+			message = "회원가입 실패";
+		}
+		System.out.println(message);
+		response.sendRedirect("../mainView/main.jsp");
 	}
 
 }
